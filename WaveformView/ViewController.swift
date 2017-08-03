7748b9cc16281b3b242f3733a8802310ae620f7a
黃柏叡 <rick.huang@phoenixnet.com.tw>
2017-08-03 13:56:32 +0800

    Initial Commit

---
+++ WaveformView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WaveformView
+//
+//  Created by 黃柏叡 on 2017/8/3.
+//  Copyright © 2017年 黃柏叡. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
